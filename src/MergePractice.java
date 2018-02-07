0d8150bd127d19aa3556ff4bf7643f0f82dce582
KassondraJones <kassondralj@yahoo.com>
2018-02-07 11:46:41 -0500

    messages here

--- src/MergePractice.java
+++ src/MergePractice.java
@@ -6,6 +6,8 @@ public class MergePractice {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("My name is moe.");
+		
+		System.out.println("This is Kassie's addition to the file.");
 	}
 
 }
