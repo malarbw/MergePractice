118d55a292489b7e2108d8036bb8b3b9931cece5
malarbw <malarbw@gmail.com>
2018-02-07 11:32:08 -0500

    first commit

---
+++ src/MergePractice.java
@@ -0,0 +1,11 @@
+/*
+ * @authout-brad malarkey
+ */
+public class MergePractice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is moe.");
+	}
+
+}
