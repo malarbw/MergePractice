327855a7d00a5b0c5938c5c340542dda8f578774
malarbw <malarbw@gmail.com>
2018-02-07 11:56:15 -0500

    added new line

--- src/MergePractice.java
+++ src/MergePractice.java
@@ -4,7 +4,6 @@
 public class MergePractice {
 
 	public static void main(String[] args) {
-		System.out.println("OOOOO");
 		System.out.println("Hello World");
 		System.out.println("My name is moe.");
 		
